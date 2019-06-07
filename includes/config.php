f778ac3c6bfa7d1b75c36f8cc4795ce5a9b8df85
jakemadness <j4kem4dness@live.com>
2019-06-07 13:55:43 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,10 +3,8 @@
 define('PATH_ABSOLUTE', dirname(__FILE__) . '/../');
 define('PATH_INCLUDES', PATH_ABSOLUTE . 'includes/');
 
-
 // require the website class.
 require_once(PATH_INCLUDES . 'website.class.php');
-
 
 // global website variables.
 $website['discord_client'] = 'discord client id';
