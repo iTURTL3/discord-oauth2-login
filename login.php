78b57837dba6d20579f11e25f70edb5f89707f8f
jakemadness <j4kem4dness@live.com>
2019-06-07 13:50:35 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+require_once('includes/config.php');
+if ( !empty($_SESSION) ) {
+   website::website_redirect($website['url']);
+}
+else if ( isset($_GET['code']) ) {
+   $auth = website::discord_auth($website['discord_client'], $website['discord_secret'], $website['discord_scopes'], $_GET['code']);
+   if ( $auth ) {
+      $user = website::discord_user_info($auth['access_token']);
+      if ( $user ) {
+         $_SESSION['discord-access-token']     = $auth['access_token'];
+         $_SESSION['discord-refresh-token']    = $auth['refresh_token'];
+         $_SESSION['discord-token-expiration'] = time() + $auth['expires_in'];
+         $_SESSION['discord-userid']           = $user['id'];
+         $_SESSION['discord-avatarid']         = $user['avatar'];
+         $_SESSION['discord-username']         = $user['username'];
+         $_SESSION['discord-discriminator']    = $user['discriminator'];
+         website::website_redirect($website['url']);
+      }
+   }
+}
+else {
+   website::discord_auth_redirect($website['discord_client'], $website['discord_scopes']);
+}
+?>
