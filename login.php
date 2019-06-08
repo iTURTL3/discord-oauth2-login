6933b1eb26347c5a7f7f1099aab3eb26db0020f4
jakemadness <j4kem4dness@live.com>
2019-06-08 10:55:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 require_once('includes/config.php');
-if ( !empty($_SESSION) ) {
+if ( isset($_SESSION['discord']) ) {
    website::website_redirect($website['url']);
 }
 else if ( isset($_GET['code']) ) {
@@ -8,13 +8,13 @@ else if ( isset($_GET['code']) ) {
    if ( $auth ) {
       $user = website::discord_user_info($auth['access_token']);
       if ( $user ) {
-         $_SESSION['discord-access-token']     = $auth['access_token'];
-         $_SESSION['discord-refresh-token']    = $auth['refresh_token'];
-         $_SESSION['discord-token-expiration'] = time() + $auth['expires_in'];
-         $_SESSION['discord-userid']           = $user['id'];
-         $_SESSION['discord-avatarid']         = $user['avatar'];
-         $_SESSION['discord-username']         = $user['username'];
-         $_SESSION['discord-discriminator']    = $user['discriminator'];
+         $_SESSION['discord']['access-token']     = $auth['access_token'];
+         $_SESSION['discord']['refresh-token']    = $auth['refresh_token'];
+         $_SESSION['discord']['token-expiration'] = time() + $auth['expires_in'];
+         $_SESSION['discord']['user-id']          = $user['id'];
+         $_SESSION['discord']['avatar-id']        = $user['avatar'];
+         $_SESSION['discord']['username']         = $user['username'];
+         $_SESSION['discord']['discriminator']    = $user['discriminator'];
          website::website_redirect($website['url']);
       }
    }
